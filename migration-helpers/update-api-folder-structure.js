1af48cf9d423d510cfd4db46fae3f643f58afdf9
Mark Kaylor <mark.kaylor@strapi.io>
2021-09-16 17:43:38 +0200

    clean up some stuff

--- migration-helpers/update-api-folder-structure.js
+++ migration-helpers/update-api-folder-structure.js
@@ -6,111 +6,153 @@ const { resolve, join } = require("path");
 const fs = require("fs-extra");
 const _ = require("lodash");
 var pluralize = require("pluralize");
-const j = require("jscodeshift");
 const { inspect } = require("util");
 
 const normalizeName = _.kebabCase;
 
-const updateContentTypes = async (apiDirCopyPath, apiName) => {
+const convertModelToContentType = async (apiPath, contentTypeName) => {
+  const settingsJsonPath = join(
+    apiPath,
+    "models",
+    `${contentTypeName}.settings.json`
+  );
+  const exists = await fs.exists(settingsJsonPath);
+  if (!exists) {
+    console.error(`${contentTypeName}.settings.json does not exist`);
+  }
+
+  // Read the settings.json file
+  const settingsJson = await fs.readJson(settingsJsonPath);
+  // Create a copy
+  const schemaJson = { ...settingsJson };
+  const infoUpdate = {
+    singularName: contentTypeName,
+    pluralName: pluralize(contentTypeName),
+    displayName: contentTypeName,
+    name: contentTypeName,
+  };
+  // Modify the JSON
+  _.set(schemaJson, "info", infoUpdate);
+  // Create the new content-types/api/schema.json file
+  await fs.ensureFile(
+    join(apiPath, "content-types", contentTypeName, "schema.json")
+  );
+  // Write modified JSON to schema.json
+  await fs.writeJSON(
+    join(apiPath, "content-types", contentTypeName, "schema.json"),
+    schemaJson,
+    {
+      spaces: 2,
+    }
+  );
+};
+
+const updateContentTypes = async (apiPath) => {
   try {
-    const schemaSettingsJson = join(
-      apiDirCopyPath,
-      apiName,
-      "models",
-      `${apiName}.settings.json`
+    const allModels = await fs.readdir(join(apiPath, "models"), {
+      withFileTypes: true,
+    });
+    const allModelFiles = allModels.filter(
+      (f) => f.isFile() && f.name.includes("settings")
     );
-    const exists = await fs.exists(schemaSettingsJson);
-    if (!exists) {
-      console.error(`${apiName}.settings.json does not exist`);
+
+    if (allModelFiles.length > 1) {
+      // loop
+      for (const model of allModelFiles) {
+        const [contentTypeName] = model.name.split(".");
+        await convertModelToContentType(apiPath, contentTypeName);
+      }
+    } else {
+      // skip the loop
+      const [contentTypeName] = allModelFiles[0].name.split(".");
+      await convertModelToContentType(apiPath, contentTypeName);
     }
 
-    // Read the settings.json file
-    const settingsJson = await fs.readJson(schemaSettingsJson);
-    // Create a copy
-    const schemaJson = { ...settingsJson };
-    const infoUpdate = {
-      singularName: apiName,
-      pluralName: pluralize(apiName),
-      displayName: apiName,
-      name: apiName,
-    };
-    // Modify the JSON
-    _.set(schemaJson, "info", infoUpdate);
-    // Create the new content-types/api/schema.json file
-    await fs.ensureFile(
-      join(apiDirCopyPath, apiName, "content-types", apiName, "schema.json")
-    );
-    // Write modified JSON to schema.json
-    await fs.writeJSON(
-      join(apiDirCopyPath, apiName, "content-types", apiName, "schema.json"),
-      schemaJson,
-      {
-        spaces: 2,
-      }
-    );
-
-    // Delete the models folder
-    await fs.remove(join(apiDirCopyPath, apiName, "models"));
+    // Delete the v3 models folder
+    await fs.remove(join(apiPath, "models"));
   } catch (error) {
     console.error(error.message);
   }
 };
 
-const updateRoutes = async (apiDirCopyPath, apiName) => {
+const updateRoutes = async (apiPath, apiName) => {
   try {
     // Create the js file
-    await fs.ensureFile(
-      join(apiDirCopyPath, apiName, "routes", `${apiName}.js`)
-    );
+    await fs.ensureFile(join(apiPath, "routes", `${apiName}.js`));
 
     // Create write stream for new js file
-    const file = fs.createWriteStream(
-      join(apiDirCopyPath, apiName, "routes", `${apiName}.js`)
-    );
+    const file = fs.createWriteStream(join(apiPath, "routes", `${apiName}.js`));
     // Get the existing JSON routes file
     const routesJson = await fs.readJson(
-      join(apiDirCopyPath, apiName, "config", "routes.json")
+      join(apiPath, "config", "routes.json")
     );
+    const { routes } = routesJson;
+
+    // Remove count
+    const updatedRoutes = routes.filter(
+      (route) => !route.handler.includes("count")
+    );
+
     // Recursively transform objects to strings
-    const routes = inspect(routesJson, { depth: Infinity });
+    const routesToString = inspect(
+      { routes: updatedRoutes },
+      { depth: Infinity }
+    );
 
     // Export routes from create js file
-    file.write(`module.exports = ${routes}`);
+    file.write(`module.exports = ${routesToString}`);
 
     // Close the write stream
     file.end();
 
-    // Delete config/routes.json
-    await fs.remove(join(apiDirCopyPath, apiName, "config", "routes.json"));
+    // Delete the v3 config/routes.json
+    await fs.remove(join(apiPath, "config", "routes.json"));
   } catch (error) {
     console.error(error);
   }
 };
 
-const updatePolicies = async (apiDirCopyPath, apiName) => {
-  const v3PoliciesPath = join(apiDirCopyPath, apiName, "config", "policies");
+const updatePolicies = async (apiPath) => {
+  const v3PoliciesPath = join(apiPath, "config", "policies");
   const exists = await fs.exists(v3PoliciesPath);
   if (!exists) return;
 
   const v3Policies = await fs.readdir(v3PoliciesPath, { withFileTypes: true });
   const policyFiles = v3Policies.filter((fd) => fd.isFile());
 
-  for (const policy of policyFiles) {
-    try {
-      await fs.copy(
-        v3PoliciesPath,
-        join(apiDirCopyPath, apiName, "policies", policy.name)
-      );
+  // The old policy folder is empty, delete it
+  if (!policyFiles.length) {
+    await fs.remove(v3PoliciesPath);
+  }
 
-      await fs.remove(join(v3PoliciesPath, policy.name));
-    } catch (error) {
-      console.error(error.message);
+  const v4PoliciesPath = join(apiPath, "policies");
+  if (policyFiles.length > 1) {
+    for (const policy of policyFiles) {
+      try {
+        await fs.copy(v3PoliciesPath, join(v4PoliciesPath, policy.name));
+        // Remove the current v3 policy
+        await fs.remove(join(v3PoliciesPath, policy.name));
+      } catch (error) {
+        console.error(error.message);
+      }
     }
+  } else {
+    await fs.copy(v3PoliciesPath, join(v4PoliciesPath, policyFiles[0].name));
+    // The last policy has been copied, delete the v3 policy folder
+    await fs.remove(v3PoliciesPath);
   }
 };
 
-const clean = () => {
-  console.log("just gotta check for empty dirs");
+const clean = async () => {
+  console.log("done");
+};
+
+const renameApiFolder = async (apiDirCopyPath) => {
+  try {
+    await fs.rename(apiDirCopyPath, "api");
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 const updateApiFolderStructure = async () => {
@@ -125,12 +167,14 @@ const updateApiFolderStructure = async () => {
 
   for (const api of apiDirs) {
     const apiName = normalizeName(api.name);
-    await updateContentTypes(apiDirCopyPath, apiName);
-    await updateRoutes(apiDirCopyPath, apiName);
-    await updatePolicies(apiDirCopyPath, apiName);
+    const apiPath = join(apiDirCopyPath, apiName);
+    await updateContentTypes(apiPath);
+    await updateRoutes(apiPath, apiName);
+    await updatePolicies(apiPath);
   }
 
   clean();
+  await renameApiFolder(apiDirCopyPath);
 };
 
 updateApiFolderStructure();
